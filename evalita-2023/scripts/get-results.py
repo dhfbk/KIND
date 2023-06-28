b24c51ca13466566f7a9db7713df02e6e79dbca4
Alessio Palmero Aprosio <alessio@apnetwork.it>
2023-06-28 11:57:53 +0200

    Added LaTeX code to the evaluation script

--- evalita-2023/scripts/get-results.py
+++ evalita-2023/scripts/get-results.py
@@ -11,6 +11,7 @@ import re
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import precision_recall_fscore_support
 from sklearn.metrics import classification_report
+import numpy as np
 
 blankNer = "O"
 prefixRe = re.compile(r"[A-Z]-")
@@ -39,34 +40,30 @@ pred_labels = loadLabels(args.pred_file)
 conversionMap = {"O": 0, "PER": 1, "LOC": 2, "ORG": 3}
 sortedLabels = ["O", "PER", "LOC", "ORG"]
 
-# labels = set()
-# labels.update(gold_labels)
-# labels.update(pred_labels)
-
-# conversionMap = {}
-# conversionMap[blankNer] = 0
-# sortedLabels = ["O"]
-# i = 0
-# for l in labels:
-#     if l != blankNer:
-#         i += 1
-#         conversionMap[l] = i
-#         sortedLabels.append(l)
-
 gold_nums = [conversionMap[v] for v in gold_labels]
 pred_nums = [conversionMap[v] for v in pred_labels]
 
 okLabels = [1, 2, 3]
 okLabelsDesc = ["PER", "LOC", "ORG"]
 print(classification_report(gold_nums, pred_nums, labels=okLabels, target_names=okLabelsDesc, zero_division=0))
+
 exit()
 
-# print(sortedLabels)
-# cm = confusion_matrix(gold_nums, pred_nums)
-# print(cm)
+# These lines print the LaTeX code for the results
+# Just comment the "exit" command to make it visible
 
-# for k in conversionMap:
-#     if k == blankNer:
-#         continue
-#     print(k)
-#     print(precision_recall_fscore_support(gold_nums, pred_nums, average="micro", labels=[conversionMap[k]]))
+micro = precision_recall_fscore_support(gold_nums, pred_nums, average='micro', labels=okLabels)[:3]
+macro = precision_recall_fscore_support(gold_nums, pred_nums, average='macro', labels=okLabels)[:3]
+results = precision_recall_fscore_support(gold_nums, pred_nums, average=None, labels=okLabels)
+results = np.delete(results, len(results) - 1, axis=0)
+results = np.transpose(results)
+
+roundAcc = 2
+tableLabels = {}
+tableLabels["Micro"] = "%s & %s & %s" % (round(micro[0], roundAcc), round(micro[1], roundAcc), round(micro[2], roundAcc))
+tableLabels["Macro"] = "%s & %s & %s" % (round(macro[0], roundAcc), round(macro[1], roundAcc), round(macro[2], roundAcc))
+for i, l in enumerate(okLabels):
+    tableLabels[l] = "%s & %s & %s" % (round(results[i][0], roundAcc), round(results[i][1], roundAcc), round(results[i][2], roundAcc))
+
+print(tableLabels[1], "&", tableLabels[2], "&", tableLabels[3], "&", tableLabels["Micro"], "&", tableLabels["Macro"])
+
